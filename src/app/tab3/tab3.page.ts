901f3f397912365674babd84e1efc39532a67925
rsemler6789 <rsemler6789@gmail.com>
2019-04-16 09:09:06 -0400

    Changed the layout of the news tab
    Made it so that a single stock entry was going to only appear once.  ie, if you had TSLA in your stocks, previously it would appear three times, once for each column.  Now it only appears once on the page.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -12,7 +12,10 @@ import { Observable } from 'rxjs';
 export class Tab3Page {
 	
 	url: string;
-    stocks:Array<any> = [];
+	stocks:Array<any> = [];
+    stocks1:Array<any> = [];
+	stocks2:Array<any> = [];
+	stocks3:Array<any> = [];
 	stocksObservable:Observable<any[]>;
 	
 	constructor(private inAppBrowser: InAppBrowser, public itemService: ItemService) {}
@@ -21,15 +24,32 @@ export class Tab3Page {
       this.stocksObservable = this.itemService.GetStockData();
       this.stocksObservable.subscribe(stocks => {
         this.stocks = stocks;
+		this.populateThreeArrays();
         if(this.stocks != undefined) {
         console.log('There are ' + this.stocks.length + ' stocks in menu.');
         console.log(this.stocks)
         }
       });
+	  
     }
 	
 	openWebPage(symbol: string) {
 		this.url = 'https://finance.yahoo.com/quote/'+symbol+'/financials?p='+symbol;
 		const browser = this.inAppBrowser.create(this.url, '_system');
+	}
+	
+	populateThreeArrays() {
+		var i;
+		for(i=0; i<this.stocks.length; i++) {
+		  if(i%3==0){
+			  this.stocks1.push(this.stocks[i]);
+		  }
+		  else if(i%3==1) {
+			  this.stocks2.push(this.stocks[i]);
+		  }
+		  else {
+			  this.stocks3.push(this.stocks[i]);
+		  }
+	  }
 	}
 }
