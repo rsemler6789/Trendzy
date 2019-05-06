a2ec6cb1263d91a9b1ce1176abd1231d89cdf673
rsemler6789 <rsemler6789@gmail.com>
2019-05-06 09:08:49 -0400

    tab 3 images are now randomly generated
    The images associated with the stocks are now randomly generated instead of being static.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -24,6 +24,9 @@ export class Tab3Page {
       this.stocksObservable = this.itemService.GetStockDataAgain();
       this.stocksObservable.subscribe(stocks => {
         this.stocks = stocks;
+		for(var i=0; i<this.stocks.length; i++) {
+			this.stocks[i].imagePath = this.generateRandomImage();
+		}
 		this.populateThreeArrays();
         if(this.stocks != undefined) {
         console.log('There are ' + this.stocks.length + ' stocks in menu.');
@@ -51,5 +54,55 @@ export class Tab3Page {
 			  this.stocks3.push(this.stocks[i]);
 		  }
 	  }
+	}
+	
+	generateRandomImage() {
+		var r = Math.floor(Math.random() * 14);
+		var retImgPath: String;
+		switch(r) {
+			case 0:
+				retImgPath = "assets/images/lemon1.png";
+				break;
+			case 1:
+				retImgPath = "assets/images/lemon2.png";
+				break;
+			case 2:
+				retImgPath = "assets/images/lemon3.png";
+				break;
+			case 3:
+				retImgPath = "assets/images/lemon4.png";
+				break;
+			case 4:
+				retImgPath = "assets/images/lemon5.png";
+				break;
+			case 5:
+				retImgPath = "assets/images/lemon6.png";
+				break;
+			case 6:
+				retImgPath = "assets/images/lemon7.png";
+				break;
+			case 7:
+				retImgPath = "assets/images/lemon8.png";
+				break;
+			case 8:
+				retImgPath = "assets/images/lemon9.png";
+				break;
+			case 9:
+				retImgPath = "assets/images/lemon10.png";
+				break;
+			case 10:
+				retImgPath = "assets/images/lemon15.jpg";
+				break;
+			case 11:
+				retImgPath = "assets/images/lemon18.jpg";
+				break;
+			case 12:
+				retImgPath = "assets/images/lemon25.jpg";
+				break;
+			default:
+				retImgPath = "assets/images/lemon19.jpg";
+				break;
+		}
+		return retImgPath;
 	}
 }
